e53ba17b80be01b72cc2a3a198e80c2382e809eb
Hung-Tao Chung <34306660+jredway@users.noreply.github.com>
2024-08-20 13:06:51 +0800

    Update TableSizeCheck.sql

---
+++ Archive/TableSizeCheck.sql
@@ -0,0 +1,22 @@
+SELECT TOP 10 
+    t.NAME AS TableName,
+    p.rows AS RowCounts,
+    SUM(a.total_pages) * 8 / 1024 AS TotalSpaceMB,
+    SUM(a.used_pages) * 8 / 1024 AS UsedSpaceMB,
+    (SUM(a.total_pages) - SUM(a.used_pages)) * 8 / 1024 AS UnusedSpaceMB
+FROM 
+    sys.tables t
+INNER JOIN      
+    sys.indexes i ON t.OBJECT_ID = i.object_id
+INNER JOIN 
+    sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
+INNER JOIN 
+    sys.allocation_units a ON p.partition_id = a.container_id
+WHERE 
+    t.NAME NOT LIKE 'dt%' AND
+    i.OBJECT_ID > 255 AND   
+    i.index_id <= 1
+GROUP BY 
+    t.NAME, p.Rows
+ORDER BY 
+    TotalSpaceMB DESC;
